291b1079ad357e4253d3427f5722554572442d76
ShadowWood <shadowwood@foxmail.com>
2018-06-12 20:26:56 +0800

    nginx config add image gzip

--- bin/asset/gen.js
+++ bin/asset/gen.js
@@ -5,8 +5,14 @@ const config = require('config');
 const srcDir = path.join(config.root, 'client/asset');
 const distDir = path.join(config.root, 'dist/asset');
 
+async function copyNowJson() {
+  const srcFile = path.join(config.root, 'now.json');
+  const distFile = path.join(config.root, 'dist/now.json');
+  await fs.copy(srcFile, distFile);
+}
 
 module.exports = async function () {
   await fs.copy(srcDir, distDir);
+  await copyNowJson();
 }
 
