aeccf8d89cbf0da9383ca3316b1aaf2165116d9d
ShadowWood <shadowwood@foxmail.com>
2018-06-27 23:40:40 +0800

    update seo files;optimize way to change language

--- client/js/header.js
+++ client/js/header.js
@@ -1,2 +1,17 @@
+import $ from 'jquery';
 
-console.log('load header test');
+$('.lan-list a').click(function() {
+  console.log('click');
+  const newLan = $(this).attr('value');
+  const oldLan = window.LOCALE_LAN;
+  let pathName = window.location.pathname;
+  const origin = window.location.origin;
+  let newHref;
+  if(pathName.indexOf(`/${oldLan}/`) > -1) {
+    newHref = `${origin}${pathName.replace(`/${oldLan}/`, `/${newLan}/`)}`;
+  } else {
+    newHref = `${origin}/${newLan}${pathName}`;
+  }
+  window.location.href = newHref;
+  console.log(newHref);
+})
